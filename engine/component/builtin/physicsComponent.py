f6e0df9b71ed55dd9e197078679d46fa418faf68
hebedebe <hebedebe1@gmail.com>
2025-07-01 10:58:56 +1000

    physics components now sync rotation

--- engine/component/builtin/physicsComponent.py
+++ engine/component/builtin/physicsComponent.py
@@ -14,12 +14,15 @@ class PhysicsComponent(Component):
         from ... import Game
         Game._instance.currentScene.addPhysics(self.actor)
         self.body.position = (*self.actor.transform.position,)
+        self.body.rotation = self.actor.transform.rotation
         return super().start()
 
     def update(self, delta_time):
         self.body.position = (*self.actor.transform.position,)
+        self.body.rotation = self.actor.transform.rotation
         return super().update(delta_time)
     
     def lateUpdate(self, delta_time):
         self.actor.transform.setPosition(self.body.position.x, self.body.position.y)
+        self.actor.transform.setRotation(self.body.angle)
         return super().lateUpdate(delta_time)
