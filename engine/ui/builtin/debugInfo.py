80d18a166b0702bcb191e2e01a9b6ecf9e740be2
hebedebe <hebedebe1@gmail.com>
2025-07-01 10:52:59 +1000

    ui uses proper default font

--- engine/ui/builtin/debugInfo.py
+++ engine/ui/builtin/debugInfo.py
@@ -4,6 +4,13 @@ from .label import Label
 class DebugInfo(Label):
     def __init__(self, scene, balls, x=10, y=40, width=300, height=60, font=None, name="debug_info"):
         rect = pygame.Rect(x, y, width, height)
+        if font is None:
+            try:
+                from ... import Game
+                game = Game._instance
+                font = game.assetManager.getDefaultFont()
+            except:
+                font = pygame.font.Font(None, 24)
         super().__init__(rect, "", font, name)
         self.scene = scene
         self.balls = balls
