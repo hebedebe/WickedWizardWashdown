39e76ac76dcc5e03a7c7e623050897ad8d610838
hebedebe <hebedebe1@gmail.com>
2025-07-06 13:19:03 +1000

    cleared main

--- main.py
+++ main.py
@@ -5,34 +5,11 @@ import pygame
 from engine.core.game import Game
 from engine.core.asset_manager import AssetManager
 
-# Builtin engine imports
-from engine.builtin.shaders import vignette_shader
-
-# Local scene imports
-from main_menu import MainMenuScene
-from game import GameScene
-from nosleep import NoSleepScene
-from jumpscare import JumpscareScene
-from gameover import GameOverScene
-from win import WinScene
-
 def main(): 
     """Run main"""
     game: Game = Game(640, 480, "Touching Jack 2: The Remake - The Sequel (Complete Edition)")
 
     AssetManager().autoloadAssets()
-    AssetManager().setDefaultFont("vcrosdmono")
-
-    game.add_postprocess_shader(vignette_shader)
-
-    game.add_scene(MainMenuScene())
-    game.add_scene(GameScene())
-    game.add_scene(NoSleepScene())
-    game.add_scene(JumpscareScene())
-    game.add_scene(GameOverScene())
-    game.add_scene(WinScene())
-
-    game.load_scene("MainMenu")
 
     game.run()
 
